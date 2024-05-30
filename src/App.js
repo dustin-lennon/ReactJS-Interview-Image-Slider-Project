0d5189d313e57def2a232f93f20f2d073338cc7c
Dustin Lennon <dustin.lennon@stelth2000inc.com>
2024-05-30 19:17:27 -0400

    Create image slider

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageSlider from './components/image-slider';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Image Slider Component */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={1} />
+    </div>
+  );
+}
+
+export default App;
